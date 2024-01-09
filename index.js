c66227cb5d7fd91ee3cebb203e356b0070f5f6ac
Fahmida Faiza <fahmidafaiza57@gmail.com>
2024-01-09 21:59:11 +0600

    added menu

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ async function run() {
     const userCollection = client.db("womenDb").collection("users");
     const dataCollection = client.db("womenDb").collection("data");
     const cartCollection = client.db("womenDb").collection("carts");
+    const menuCollection = client.db("womenDb").collection("menu");
     app.get("/data", async (req, res) => {
       const result = await dataCollection.find().toArray();
       res.send(result);
@@ -149,6 +150,55 @@ async function run() {
     });
 
     // //////////
+  // menu related aoi
+ app.get("/menu", async (req, res) => {
+ 
+   const result = await menuCollection.find().toArray();
+   res.send(result);
+ });
+
+ app.post("/menu", verifyToken,verifyAdmin,async (req, res) => {
+   const item = req.body;
+   const result = await menuCollection.insertOne(item);
+   res.send(result);
+ });
+  app.get("/menu/:id", async (req, res) => {
+    const id = req.params.id;
+    const query = { _id: new ObjectId(id) };
+    const result = await menuCollection.findOne(query);
+    res.send(result);
+  });
+
+    app.patch("/menu/:id", async (req, res) => {
+      const item = req.body;
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          name: item.name,
+          category: item.category,
+          price: item.price,
+          recipe: item.recipe,
+          image: item.image,
+        },
+      };
+
+      const result = await menuCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    });
+
+  app.delete("/menu/:id", verifyToken, verifyAdmin, async (req, res) => {
+    const id = req.params.id;
+    const query = { _id: new ObjectId(id) };
+    const result = await menuCollection.deleteOne(query);
+    res.send(result);
+  });
+
+
+
+
+
+    ////////////////////
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
